9ce911edfa70b0efc7e623b7234b2f965d94f81a
Lionel Herrero Battista <36281552+lionelherrerobattista@users.noreply.github.com>
2018-04-26 11:03:52 -0300

    Add files via upload

--- GerFlix 1.0/main.c
+++ GerFlix 1.0/main.c
@@ -6,6 +6,10 @@
 
 int main()
 {
+    int i;
+    int j;
+    int index;
+
     eSerie listaDeSeries[TAMSERIE];
     eUsuario listaDeUsuarios[TAMUSUARIO];
 
@@ -14,6 +18,33 @@ int main()
 
     inicializarUsuariosEstado(listaDeUsuarios,TAMUSUARIO);
     inicializarUsuariosHardCode(listaDeUsuarios);
+
+    for (i=0; i<5; i++)
+    {
+      printf("%d %10s %10s %10d %10d\n", listaDeSeries[i].idSerie, listaDeSeries[i].nombre, listaDeSeries[i].genero, listaDeSeries[i].cantidadTemporadas, listaDeSeries[i].estado);
+    }
+
+    printf(" \n");
+
+    for (i=0; i<15; i++)
+    {
+
+      index=listaDeUsuarios[i].idSerie;
+
+      for (j=0; j<5; j++)
+      {
+          if (index == listaDeSeries[j].idSerie)
+          {
+              serie=listaDeSeries==
+          }
+
+      }
+
+
+
+      printf("%d %10s %10d %10d\n", listaDeUsuarios[i].idUsuario, listaDeUsuarios[i].nombre, listaDeUsuarios[i].idSerie, listaDeUsuarios[i].estado);
+    }
+
 
 /*  1. Mostrar el listado de series
     2. Mostrar el listado de usuarios
